f28ba8394919a0b8c8089f49e1022a29afe1a53a
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2023-11-20 10:58:02 -0300

    swaping turns

--- scripts.js
+++ scripts.js
@@ -1,15 +1,53 @@
 const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('[data-board]');
 
-for(const cell of cellElements){
-    cell.addEventListener("click", handleClick, {once: true});
+let isCircleTurn;
+
+const startGame = () => {
+    for(const cell of cellElements){
+        cell.addEventListener("click", handleClick, {once: true});
+    }
+
+    isCircleTurn = false;
+
+    board.classList.add('x');
 }
 
-const handleClick = () => {
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+}
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    board.classList.remove('circle');
+    board.classList.remove('x');
+
+    if(isCircleTurn){
+        board.classList.add('circle');
+    }
+    else {
+        board.classList.add('x');
+    }
+}
+
+const handleClick = (e) => {
     // put X or O
+    const cell = e.target;
+    const classToAdd = isCircleTurn ? 'circle' : 'x';
+
+    placeMark(cell, classToAdd);
 
     // verify winning
 
     // verify draw
 
     // change next player
+    swapTurns();
 }
+
+for(const cell of cellElements){
+    cell.addEventListener("click", handleClick, {once: true});
+}
+
+startGame();
